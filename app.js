afc6ce3b57ecc4b3f747a8f8a20067fc4f3a9314
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-31 12:00:49 +0500

    node.js 5

---
+++ app.js
@@ -0,0 +1,9 @@
+import fs from "node:fs";
+import fsP from "node:fs/promises";
+import path from "node:path";
+
+const dataPath = path.join(process.cwd(), "user.json");
+fsP
+  .readFile(dataPath, "utf-8")
+  .then((data) => console.log(data))
+  .catch((data) => console.log("Xatolik"));
---
+++ test.js
@@ -0,0 +1,24 @@
+import fs from "node:fs";
+import fsPromise from "node:fs/promises";
+import path from "node:path";
+
+// fs.writeFile(
+//   path.join(process.cwd(), "test1.txt"),
+//   "Hello GitHub!",
+//   (err, data) => {
+//     console.log("1");
+//   }
+// );
+// fs.writeFileSync(path.join(process.cwd(), "test2.txt"), "Hello GitHub!");
+// console.log("start 1");
+// fs.readFile(path.join(process.cwd(), "test2.txt"), "utf-8", (err, data) => {
+//   console.log("1:", data);
+// });
+// console.log("end 1");
+
+
+
+// console.log("start 2");
+// let data = fs.readFileSync(path.join(process.cwd(), "test2.txt"), "utf-8");
+// console.log("2:", data);
+// console.log("end 2");
