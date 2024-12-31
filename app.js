e43dd437fe41eb809bc64bda149abaf2616b4dcd
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-31 12:01:30 +0500

    node.js 5

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-import fs from "node:fs";
-import fsP from "node:fs/promises";
-import path from "node:path";
+// import fs from "node:fs";
+// import fsP from "node:fs/promises";
+// import path from "node:path";
 
-const dataPath = path.join(process.cwd(), "user.json");
-fsP
-  .readFile(dataPath, "utf-8")
-  .then((data) => console.log(data))
-  .catch((data) => console.log("Xatolik"));
+// const dataPath = path.join(process.cwd(), "users.json");
+// fsP
+//   .readFile(dataPath, "utf-8")
+//   .then((data) => console.log(data))
+//   .catch((data) => console.log("Xatolik"));
+
+// * stream : (protocal: (TCP:100%  and UTP: 'd..d..d.d'))
+
+import { EventEmitter } from "node:events";
+import { Stream } from "node:stream";
+const eventEmitter = new EventEmitter();
